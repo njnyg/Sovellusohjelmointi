dab740a0ebe07e9f4f79c969b8c063bc0e0ba433
Nina <nina.nygren99@gmail.com>
2024-09-17 20:02:36 +0300

    commented some blocks of code for future exercises

--- CH09/Band.py
+++ CH09/Band.py
@@ -6,8 +6,8 @@ class Band:
         self.number_of_gold_records = 0
         self.number_of_platinum_records = 0
 
-    #def describe_band(self, band_name, music_genre):
-    #    print(f'Name: {self.band_name}, Genre: {self.music_genre}')
+    """def describe_band(self, band_name, music_genre):
+        print(f'Name: {self.band_name}, Genre: {self.music_genre}')"""
 
     def is_active(self):
         print(f'{self.band_name} is active.')
@@ -23,12 +23,11 @@ class Band:
         self.number_of_platinum_records = number_of_platinum_records
     
 
-
-band = Band('System of a Down', 'Progressive Metal')
+"""band = Band('System of a Down', 'Progressive Metal')
 
 band.set_number_of_gold_records(5)
 print(f'Gold records: {band.number_of_gold_records}')
 
 band.set_number_of_platinum_records(5)
-print(f'Platinum records: {band.number_of_platinum_records}')
+print(f'Platinum records: {band.number_of_platinum_records}')"""
 
