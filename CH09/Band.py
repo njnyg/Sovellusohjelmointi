4a64defe7c95e1db6d99219fb10c431c837ac8eb
Nina <nina.nygren99@gmail.com>
2024-09-17 18:52:56 +0300

    Finished class Band

--- CH09/Band.py
+++ CH09/Band.py
@@ -1,21 +1,34 @@
 class Band:
 
-    def __init__(self, band_name, music_genre, status):
+    def __init__(self, band_name, music_genre, number_of_gold_records = 0, number_of_platinum_records = 0):
         self.band_name = band_name
         self.music_genre = music_genre
-        self.status = status
+        self.number_of_gold_records = number_of_gold_records
+        self.number_of_platinum_records = number_of_platinum_records
 
-    def describe_band(self, band_name, music_genre):
-        print(f'{band_name} is a {music_genre} band.')
+    #def describe_band(self, band_name, music_genre):
+    #    print(f'Name: {self.band_name}, Genre: {self.music_genre}')
 
-    def is_active(self, band_name, status):
-        print(f'{band_name} is {status}.')
+    def is_active(self):
+        print(f'{self.band_name} is active.')
 
     # This method returns the object instance in string format
     def __str__(self):
-        return self.band_name, self.music_genre
+        return f'Name: {self.band_name}, Genre: {self.music_genre}'
+    
+    def set_number_of_gold_records(self, number_of_gold_records):
+        self.number_of_gold_records = number_of_gold_records
+    
+    def set_number_of_platinum_records(self, number_of_platinum_records):
+        self.number_of_platinum_records = number_of_platinum_records
+    
 
 
-band1 = Band('band1', 'rock', 'active')
+band = Band('System of a Down', 'Progressive Metal')
 
-print(f'Describe band: {band1.__str__()}')
+band.set_number_of_gold_records(5)
+print(f'Gold records: {band.number_of_gold_records}')
+
+band.set_number_of_platinum_records(5)
+print(f'Platinum records: {band.number_of_platinum_records}')
+
